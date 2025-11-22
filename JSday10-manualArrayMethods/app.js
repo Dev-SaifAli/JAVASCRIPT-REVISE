c649e437bafd2f68dd32d164ce1c6bff8709092e
SAIF ALI RABBANI <saifu100@outlook.com>
2025-11-22 22:30:26 +0500

    Implement manual push functions and high-order function for adding elements to an array

--- JSday10-manualArrayMethods/app.js
+++ JSday10-manualArrayMethods/app.js
@@ -1,5 +1,6 @@
 // Popped Element Function
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
 const poppedElement = function (arr) {
   arr.length = arr.length - 1;
   return arr;
@@ -25,3 +26,31 @@ function printArray(arr) {
 }
 
 removeLastElement(arr, printArray);
+
+// Pushed Element Function
+const pushedElement1 = function (arr, element) {
+  arr[arr.length] = element;
+  // return arr;
+};
+pushedElement1(arr, 10);
+console.log("Array after push:", arr);
+
+const pushedElement2 = function (arr, element) {
+  for (let i = arr.length; i < arr.length + 1; i++) {
+    arr[i] = element;
+    break;
+  }
+};
+pushedElement2(arr, 16);
+console.log("Array after second push:", arr);
+// High-Order Function for Push
+const pushElement = function (arr, element, callback) {
+  callback(arr, element);
+};
+const addElement = function (arr, element) {
+  arr[arr.length] = element;
+};
+
+pushElement(arr, "newArray", addElement);
+console.log("\n");
+console.log("Array after HOF Push:", arr);
