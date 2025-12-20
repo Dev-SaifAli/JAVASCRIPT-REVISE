49708b46fc49bbc5bd64b916182b0dce2ed926bf
SAIF ALI RABBANI <saifu100@outlook.com>
2025-12-21 03:09:47 +0500

    Add form for student details entry and implement local storage functionality

---
+++ formWithTable/js/record.js
@@ -0,0 +1,45 @@
+let records = JSON.parse(localStorage.getItem("records") ?? JSON.stringify([]));
+
+const ROOT = document.getElementById("table-root");
+const table = document.createElement("table");
+const tHead = document.createElement("thead");
+const tBody = document.createElement("tbody");
+
+const headers = ["name", "age", "dob", "gender", "Actions"];
+headers.forEach(function (header) {
+  const th = document.createElement("th");
+  th.textContent = header;
+  tHead.appendChild(th);
+});
+
+records.forEach(function (element) {
+  const tr = document.createElement("tr");
+
+  Object.values(element).forEach(function (text) {
+    const td = document.createElement("td");
+    td.textContent = text;
+    tr.appendChild(td);
+  });
+
+  const actionTd = document.createElement("td");
+  const dltButton = document.createElement("button");
+
+  dltButton.textContent = "Delete";
+  dltButton.setAttribute("id", "delete");
+
+  dltButton.addEventListener("click", function () {
+    const rows = [...tBody.querySelectorAll("tr")];
+    const rowIndex = rows.indexOf(tr);
+    if (rowIndex !== -1) {
+      tr.remove();
+      records.splice(rowIndex, 1);
+      localStorage.setItem("records", JSON.stringify(records));
+    }
+  });
+  actionTd.appendChild(dltButton);
+  tr.appendChild(actionTd);
+  tBody.appendChild(tr);
+});
+table.appendChild(tHead);
+table.appendChild(tBody);
+ROOT.appendChild(table);
